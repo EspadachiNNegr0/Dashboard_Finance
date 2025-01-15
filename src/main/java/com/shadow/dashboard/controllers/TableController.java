65f41322170f74cc6b6cfa9df22ee9e28c4b4268
Eduardo Santos Gomes ferreira <106843885+EspadachiNNegr0@users.noreply.github.com>
2025-01-15 19:26:46 -0300

    Delete src directory

--- src/main/java/com/shadow/dashboard/controllers/TableController.java
+++
@@ -1,4 +0,0 @@
-package com.shadow.dashboard.controllers;
-
-public class TableController {
-}
--- src/main/java/com/shadow/dashboard/models/Status.java
+++
@@ -1,6 +0,0 @@
-package com.shadow.dashboard.models;
-
-public enum Status {
-
-    PROCESSING,COMPLETE, FAILED
-}
